05d684de7172cc369581485391d57a4a6b28758f
Nazrina <nazrinakhan88@gmail.com>
2024-07-21 13:54:01 -0400

    ansible variables

--- infracreate/main.tf
+++ infracreate/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "tool" {
     instance_type = var.instance_type
     vpc_security_group_ids = [aws_security_group.tool-sg.id]
     tags = {
-            Name = var.name
+        Name = var.name
     }
     }
 resource "aws_security_group" "tool-sg" {
