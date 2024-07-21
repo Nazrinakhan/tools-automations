4b7dec887617602e5176eb4f3a1aa7f4b812b053
Nazrina <nazrinakhan88@gmail.com>
2024-07-21 14:20:18 -0400

    ansible variables

--- infracreate/main.tf
+++ infracreate/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "tool" {
   ami                    = data.aws_ami.rhel9.image_id
-  instance_type          = "t3.micro"
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.tool-sg.id]
 
   tags = {
