4cb603d74931b0a1810db5f20dcdea1574219833
Nazrina <nazrinakhan88@gmail.com>
2024-07-21 13:58:57 -0400

    ansible variables

--- infracreate/main.tf
+++ infracreate/main.tf
@@ -1,25 +1,27 @@
 resource "aws_instance" "tool" {
     ami               = data.aws_ami.rhel9.image_id
-    instance_type = var.instance_type
+    instance_type     = var.instance_type
     vpc_security_group_ids = [aws_security_group.tool-sg.id]
+
     tags = {
         Name = var.name
     }
-    }
+}
+
 resource "aws_security_group" "tool-sg" {
   name        = "$(var.name)-sg"
   description = "$(var.name)-sg"
 
 
 
-    egress {
+   egress {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
 
   }
-    ingress {
+   ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "TCP"
@@ -28,4 +30,4 @@ resource "aws_security_group" "tool-sg" {
      tags = {
         Name = "${var.name}-sg"
     }
-    }
+}
