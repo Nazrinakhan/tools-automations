c50d22104d88a1ea7d443bb88989519d0d0637f0
Nazrina <nazrinakhan88@gmail.com>
2024-07-22 19:59:42 -0400

    ansible variables

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "tool-infra-create" {
     for_each = var.tools
-    source = "./infra-create"
+    source = "./infracreate"
     name = each.key
     instance_type = each.value["instance_type"]
     }
